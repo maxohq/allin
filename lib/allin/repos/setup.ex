a266401a34d1e8db64f20c9fa2a72514b4d8857b
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-21 14:42:17 +0200

    Fix: do not start the repo directly, let the supervisor handle it

--- lib/allin/repos/setup.ex
+++ lib/allin/repos/setup.ex
@@ -3,9 +3,9 @@ defmodule Allin.Repos.Setup do
 
   def setup_repo! do
     assert_correct_db_type!()
-    if dbtype?("mysql"), do: to_mysql()
-    if dbtype?("psql"), do: to_psql()
-    if dbtype?("sqlite"), do: to_sqlite()
+    if dbtype?("mysql"), do: to_mysql(false)
+    if dbtype?("psql"), do: to_psql(false)
+    if dbtype?("sqlite"), do: to_sqlite(false)
   end
 
   def assert_correct_db_type! do
@@ -17,13 +17,13 @@ defmodule Allin.Repos.Setup do
   def dbtype, do: System.get_env("DBTYPE")
   def dbtype?(type), do: dbtype() == type
 
-  def to_mysql, do: setup_repo(:myxql, Allin.RepoMysql)
-  def to_psql, do: setup_repo(:postgrex, Allin.RepoPsql)
-  def to_sqlite, do: setup_repo(:exqlite, Allin.RepoSqlite)
+  def to_mysql(boot \\ true), do: setup_repo(:myxql, Allin.RepoMysql, boot)
+  def to_psql(boot \\ true), do: setup_repo(:postgrex, Allin.RepoPsql, boot)
+  def to_sqlite(boot \\ true), do: setup_repo(:exqlite, Allin.RepoSqlite, boot)
 
-  defp setup_repo(db_app, repo_mod) do
+  defp setup_repo(db_app, repo_mod, start_repo) do
     :application.ensure_all_started(db_app)
-    repo_mod.start_link()
+    if start_repo, do: repo_mod.start_link()
     Allin.Repo.configure(repo_mod)
   end
 end
